4cbf0b07026051692144af1bcad7846efeaaf7aa
João Nodari <joaonodari2017@hotmail.com>
2025-01-17 17:57:38 -0300

    Inicialização do Projeto.

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const btnNext = document.querySelector('.next')
+const btnBack = document.querySelector('.back')
+
+const container = document.querySelector('.container')
+const list = document.querySelector('.list')
+const thumb = document.querySelector('.thumb')
+
+btnNext.onclick = () => moveItemsOnclick('next')
+btnBack.onclick = () => moveItemsOnclick('back')
+
+function moveItemsOnclick(type) {
+    if(type === 'next') {
+        
+    }
+}
