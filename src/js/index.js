31f8d13840b4b6d0ba9a30eb738f3129ca154caf
João Nodari <joaonodari2017@hotmail.com>
2025-01-18 10:49:31 -0300

    Implementando funcionalidade.

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,17 @@ btnNext.onclick = () => moveItemsOnclick('next')
 btnBack.onclick = () => moveItemsOnclick('back')
 
 function moveItemsOnclick(type) {
+
+    const listItems = document.querySelectorAll('.list-item')
+    const thumbItems = document.querySelectorAll('.thumb-item')
+    
     if(type === 'next') {
-        
+        list.appendChild(listItems[0])
+        thumb.appendChild(thumbItems[0])
+    } else {
+        list.prepend(listItems[listItems.length - 1])
+        thumb.prepend(thumbItems[thumbItems.length - 1])
     }
 }
+
+console.log(list)
